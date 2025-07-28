34df95cff9479219f3d1756812083185d1493381
vinhlaem <vinhlaem2000@gmail.com>
2025-07-28 16:29:01 +0700

    add amchart

---
+++ src/pages/abc.tsx
@@ -0,0 +1,15 @@
+// import Chart from "@/components/Chart";
+import dynamic from "next/dynamic";
+
+const Chart = dynamic(() => import("@/components/Chart"), {
+  ssr: false,
+});
+
+export default function ABC() {
+  return (
+    <div className="flex flex-col items-center justify-center" style={{ backgroundColor: "white", height: "100vh" }}>
+      <h1 className="text-4xl font-bold">ABC</h1>
+      <Chart />
+    </div>
+  );
+}
